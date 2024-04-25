2370d8eee250b5b8ff6116bad769b1a053762d68
augusto406 <168099093+augusto406@users.noreply.github.com>
2024-04-25 20:49:20 -0300

    Add files via upload

---
+++ par ou impar.py
@@ -0,0 +1,6 @@
+numero = int(input('me diga um numero qualquer: '))
+resultado = numero % 2
+if resultado == 0:
+    print(f'o {numero} é um número par!')
+else:
+    print(f'o {numero} é um número ímpar!')
